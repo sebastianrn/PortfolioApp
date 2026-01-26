e15d8661525082833ce686aff1d4a4334cabc7d0
Sebastian Reissmann <sebastian.reissmann@swisscom.com>
2026-01-26 13:55:29 +0100

    build(release): Bump version to 1.0
    This commit updates the application's version from "0.2" to "1.0", signaling a major release milestone.
    
    **Key Changes:**
    
    *   **`app/build.gradle.kts`**:
        *   The `versionName` has been changed from `"0.2"` to `"1.0"`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 2
-        versionName = "0.2"
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
